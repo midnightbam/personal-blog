7fbe8e94ddbc531d329cdc2366ca8de7f9cf4ecb
Bambam <punyanuch.ksd@gmail.com>
2025-10-12 16:29:36 +0700

    feat: initial setup with Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <section className="p-8 text-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        My First React App on Vite
+      </h1>
+    </section>
+  );
+}
+
+export default App;
